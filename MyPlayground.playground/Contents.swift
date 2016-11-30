73ffb82943988da697da2b24a3feb96cabd867fa
Daniel Chang <daniel.chang@daniel-change-macbook.local>
2016-11-30 16:36:46 +0800

    20161130:  Save

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -125,3 +125,37 @@ func greet(_ person: String, on day: String) -> String {
 }
 greet("John", on: "Wednesday")
 
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        }
+        else if score < min {
+            min = score
+        }
+        sum += score
+    }
+    
+    return (min, max, sum)
+}
+
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+print(statistics.2)
+
+func sumOf(numbers: Int...) -> Int {
+    var sum = 0
+    for number in numbers {
+        sum += number
+    }
+    return sum
+}
+sumOf()
+sumOf(numbers: 42, 597, 12)
+
+
